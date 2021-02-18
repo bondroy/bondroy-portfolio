4f696cc550d4b4b2790972885d4c5d60accfa94f
Maki <loudnownet@gmail.com>
2021-02-18 12:32:43 +0100

    mobile services

--- src/_includes/services/index.js
+++ src/_includes/services/index.js
@@ -14,26 +14,9 @@ function Services (el) {
   })
 
   function onProgress (progress, layers) {
+    if (window.matchMedia('(max-width: 1040px)').matches) return
     layers.forEach(layer => {
       layer.el.style.transform = `translateY(${layer.y * progress}rem) translateZ(0)`
     })
   }
-
-  // screenProgress.onProgress((progress) => {
-  //   img.style.transform = `translateY(-80vh) translateY(${150 * progress}vh) translateZ(0) scale(1.2)`
-
-  //   if (progress < .3 || progress >= .8) {
-  //     content.style.opacity = 0
-  //   }  else {
-  //     content.style.opacity = 1
-  //   }
-
-  //   screenProgress.step(progress, .3, .4, (progress) => {
-  //     content.style.opacity = progress
-  //   })
-
-  //   screenProgress.step(progress, .7, .8, (progress) => {
-  //     content.style.opacity = 1 - progress
-  //   })
-  // })
 }
