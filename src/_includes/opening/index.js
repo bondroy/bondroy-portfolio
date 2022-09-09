afef26a6c09d59fcdea2f89063d6988d5b04affb
Bartosz Makos <bartosz.makos@xwp.co>
2022-09-09 12:03:48 +0200

    job opening

--- src/_includes/opening/index.js
+++ src/_includes/opening/index.js
@@ -1,48 +1,38 @@
 function OpeningFooter (el, container) {
-  const footer = document.createDocumentFragment()
-  footer.innerHTML = el.outerHTML
+  const stickyFooter = document.createElement('div')
+  stickyFooter.classList.add('opening__sticky-footer')
+  stickyFooter.innerHTML = el.outerHTML
+  document.querySelector('body').appendChild(stickyFooter)
 
-  document.body.appendChild(footer)
+  let containerEnd
+  let scrolledToBottom
 
-  console.log(footer.children)
-  // let footerOffset = 0
-  // let height
-  // let containerEnd
-  // let scrolledToBottom
+  setVars()
+  window.addEventListener('DOMContentLoaded', setVars)
+  window.addEventListener('resize', setVars)
+  window.addEventListener('load', setVars)
 
-  // setVars()
-  // window.addEventListener('DOMContentLoaded', setVars)
-  // window.addEventListener('resize', setVars)
-  // window.addEventListener('load', setVars)
+  window.renderer.onUpdate((offset) => {
+    if (window.matchMedia('(max-width: 1040px)').matches) return
 
-  // window.renderer.onUpdate((offset) => {
-  //   if (window.matchMedia('(max-width: 1040px)').matches) return
+    scrolledToBottom = offset + window.innerHeight >= containerEnd
+    console.log(scrolledToBottom)
+    if (!scrolledToBottom) {
+      stickyFooter.style.transform = 'translateY(0)'
+    }
+  })
 
-  //   scrolledToBottom = offset + window.innerHeight >= containerEnd
-  //   if (!scrolledToBottom) {
-  //     el.classList.remove('-bottom')
-  //     footerOffset = Math.min(offset, height)
-  //     footerOffset = Math.max(footerOffset, 0)
-  //   }
-  // })
+  window.renderer.onRender(() => {
+    if (window.matchMedia('(max-width: 1040px)').matches) return
 
-  // // Add secondary footer from js / outside of scroller
-  // // stripes
+    if (scrolledToBottom) {
+      stickyFooter.style.transform = 'translateY(100%)'
+    } else {
+      stickyFooter.style.transform = 'translateY(0)'
+    }
+  })
 
-  // window.renderer.onRender(() => {
-  //   if (window.matchMedia('(max-width: 1040px)').matches) return
-
-  //   if (scrolledToBottom) {
-  //     el.classList.add('-bottom')
-  //   } else {
-  //     el.classList.remove('-bottom')
-  //     console.log(footerOffset)
-  //     el.style.transform = 'translateY(-' + footerOffset + 'px) translateZ(0)'
-  //   }
-  // })
-
-  // function setVars () {
-  //   height = el.offsetHeight
-  //   containerEnd = container.offsetHeight + container.offsetTop
-  // }
+  function setVars () {
+    containerEnd = container.offsetHeight + container.offsetTop
+  }
 }
